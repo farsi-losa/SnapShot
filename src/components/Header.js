d9c10ab9dc890a9e72ddb68090e2de58f5a5c9a3
farsi-losa <farsi.losa@ovo.id>
2020-09-14 16:38:39 +0700

    remove snapshot

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import Navigation from "./Navigation";
 const Header = ({ history, handleSubmit }) => {
   return (
     <div>
-      <h1>SnapShot</h1>
       <Form history={history} handleSubmit={handleSubmit} />
       <Navigation />
     </div>
